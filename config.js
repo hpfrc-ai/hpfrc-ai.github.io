939998b7e074f5d1e8b6dacaed10f8838ab05772
hpfrc-ai <kosm651@gmail.com>
2026-01-18 22:00:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    MY_KEY : 'AIzaSyCGOGhKcFNX69Vm-CrIZE3tea2hcvQna88',
+    MY_KEY : 'AIzaSyCu8Ep4js6hpjKBnjxmZzGEm4_3CK5c1po',
     SECRET_KEY : '56789',
     KEY_2 : '101010'
   }
